b30d1fb8496560b51c042dd9dfcbe4163ae8dac7
Felipe Ryan <fryan@fryan-ltm3.internal.salesforce.com>
2018-05-30 13:36:36 +0200

    refactor

--- server_flask_websocket.py
+++ server_flask_websocket.py
@@ -2,12 +2,9 @@ from flask import Flask, render_template, jsonify, request, abort
 from flask_socketio import SocketIO, emit
 import json
 from corsdecorator import crossdomain
-
-from small_airport import *
 import time
 
-from thread_test import *
-
+from threaded_airport_simulator import *
 import kafka_helper
 
 app = Flask(__name__)
@@ -23,24 +20,6 @@ def hello():
     return render_template('index.html')
 
 
-@app.route('/api/v1.0/scans', methods=['POST', 'OPTIONS'])
-@crossdomain(origin='*')
-def receive_scan_from_api():
-    if not request.json:
-        return 'not json...'
-
-    msg = json.dumps(request.json)
-
-    print ('\nJson Received from External Source:')
-    print (msg)
-    print ('\n')
-    
-    socketio.emit('scan', msg)
-
-    return jsonify({'Response':'All ok'}), 201
-
-
-# Actual Socket Events
 @socketio.on('ping')
 def handle_my_custom_event(jsonMessage):
     print('received json: ' + str(jsonMessage))
@@ -48,67 +27,63 @@ def handle_my_custom_event(jsonMessage):
     emit('pong', reply)
 
 
-# Actual Socket Events
-@socketio.on('go')
-def handle_start(jsonMessage):
-    print('received json: ' + str(jsonMessage))
-    reply = json.dumps({'message':'Starting'})
-    emit('Starting', reply)
-
-    for i in range(1, 300):
-        time.sleep(1)
-        
-        if i % 50 == 0:
-            add_block()
-            time.sleep(1)
-        
-        move_list = airport.move_all()
-        moves = [m.pos() for m in move_list]
-
-        print(i)
-
-        socketio.emit('scan', json.dumps({'message': moves}))
-#
-#
+# One Kafka event per second with all movement
+# this will create a small number of Kadka events per second.
 @socketio.on('start')
 def threaded_handle_start(jsonMessage):
 
     reply = json.dumps({'message':'Starting'})
     emit('Starting', reply)
-    
-    # global tt
 
     for t in threads:
         t.event.set()
     
     tt = TimerClass(socketio, True)
     threads.append(tt)
+    
+    # The thread itself emits the necessary data to the socket.
     tt.start()
 
 
+# One Kafka event per "person" movement, this will generate a 
+# larger number of Kafka events per second.
 @socketio.on('flood')
 def threaded_handle_flood(jsonMessage):
 
     reply = json.dumps({'message':'Starting'})
     emit('Starting - FLOOD', reply)
-    
-    # global tt
 
     for t in threads:
         t.event.set()
     
     tt = TimerClass(socketio, False)
     threads.append(tt)
+
+    # The thread itself emits the necessary data to the socket.
     tt.start()
 
 
 @socketio.on('stop')
 def stop_thread(jsonMessage):
     print('\nStop Thread\n')
-    # if tt and tt.isAlive():
-    
     for t in threads:
         t.event.set()
+
+
+# left in here for example purposes, in case you want to get a POST
+# and a json payload and emit a socketio message back to subscribing
+# clients.
+@app.route('/api/v1.0/scans', methods=['POST', 'OPTIONS'])
+@crossdomain(origin='*')
+def receive_scan_from_api():
+    if not request.json:
+        return 'not json...'
+    msg = json.dumps(request.json)
+    print ('\nJson Received from External Source:')
+    print (msg)
+    print ('\n') 
+    socketio.emit('post', msg)
+    return jsonify({'Response':'All ok'}), 201
 
 
 if __name__ == '__main__':
